5f441c138a27fbe8e5743c4953225bd4d9182c4a
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-04-22 21:53:54 +0200

    Initial Commit

---
+++ Layout and Geometry/Layout_and_GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_and_GeometryApp.swift
+//  Layout and Geometry
+//
+//  Created by charlene hoareau on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Layout_and_GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
